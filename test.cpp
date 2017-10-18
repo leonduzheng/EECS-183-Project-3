acf06005da06cbf65aa9879cba469dbdb3f4e1a0
nweingarden <lightningtyphoon@gmail.com>
2017-10-18 17:57:20 -0400

    Added checker functions

--- test.cpp
+++ test.cpp
@@ -16,11 +16,17 @@
 #include "ohhi.h"
 
 void test_count_unknown_squares();
+void test_row_has_no_threes_of_color();
+void test_col_has_no_threes_of_color();
+void test_board_has_no_threes();
 
 // declare more test functions here
 
 int main() {
     test_count_unknown_squares();
+    test_row_has_no_threes_of_color();
+    test_col_has_no_threes_of_color();
+    test_board_has_no_threes();
 
     // add calls to test functions here
 
@@ -29,6 +35,7 @@ int main() {
 
 
 void test_count_unknown_squares() {
+    cout << "count_unknown_squares()" << endl;
     int board[MAX_SIZE][MAX_SIZE];
 
     // test case 1
@@ -41,6 +48,137 @@ void test_count_unknown_squares() {
     cout << count_unknown_squares(board, size_1) << endl;
 
     // add more tests here
+    
+    // test case 2
+    string test_board_2[] = {"----",
+                             "----",
+                             "----",
+                             "----"};
+    int size_2 = 4;
+    read_board_from_string(board, test_board_2, size_2);
+    cout << count_unknown_squares(board, size_2) << endl;
+    
 }
+
+void test_row_has_no_threes_of_color() {
+    cout << "row_has_no_threes_of_color()" << endl;
+    int board[MAX_SIZE][MAX_SIZE];
+
+    // test case 1
+
+    string test_board_1[] = {"O-OX",
+                             "OO--",
+                             "X---",
+                             "-O--"};
+    int size_1 = 4;
+    read_board_from_string(board, test_board_1, size_1);
+    cout << row_has_no_threes_of_color(board, size_1, 0, BLUE) << endl;
+
+    // test case 2
+    string test_board_2[] = {"OOOX",
+                             "OO--",
+                             "X---",
+                             "-O--"};
+    int size_2 = 4;
+    read_board_from_string(board, test_board_2, size_2);
+    cout << row_has_no_threes_of_color(board, size_2, 0, BLUE) << endl;
+
+    // test case 3
+    string test_board_3[] = {"OOOX",
+                             "OXXX",
+                             "X---",
+                             "-O--"};
+    int size_3 = 4;
+    read_board_from_string(board, test_board_3, size_3);
+    cout << row_has_no_threes_of_color(board, size_3, 1, RED) << endl;
+}
+
+void test_col_has_no_threes_of_color() {
+    cout << "col_has_no_threes_of_color()" << endl;
+    int board[MAX_SIZE][MAX_SIZE];
+
+    // test case 1
+
+    string test_board_1[] = {"O-OX",
+                             "OO--",
+                             "X---",
+                             "-O--"};
+    int size_1 = 4;
+    read_board_from_string(board, test_board_1, size_1);
+    cout << col_has_no_threes_of_color(board, size_1, 0, BLUE) << endl;
+
+    // test case 2
+    string test_board_2[] = {"O-OX",
+                             "OO--",
+                             "O---",
+                             "-O--"};
+    int size_2 = 4;
+    read_board_from_string(board, test_board_2, size_2);
+    cout << col_has_no_threes_of_color(board, size_2, 0, BLUE) << endl;
+
+    // test case 3
+    string test_board_3[] = {"OOOX",
+                             "OXXX",
+                             "X--X",
+                             "-O-O"};
+    int size_3 = 4;
+    read_board_from_string(board, test_board_3, size_3);
+    cout << col_has_no_threes_of_color(board, size_3, 3, RED) << endl;
+
+    // test case 4
+    string test_board_4[] = {"O-OX",
+                             "OO--",
+                             "O---",
+                             "-O--"};
+    int size_4 = 4;
+    read_board_from_string(board, test_board_4, size_4);
+    cout << col_has_no_threes_of_color(board, size_4, 0, BLUE) << endl;
+
+    // test case 5
+    string test_board_5[] = {"OOOX",
+                             "OO--",
+                             "X---",
+                             "-O--"};
+    int size_5 = 4;
+    read_board_from_string(board, test_board_5, size_5);
+    cout << col_has_no_threes_of_color(board, size_5, 0, BLUE) << endl;
+    
+}
+
+void test_board_has_no_threes() {
+    cout << "board_has_no_threes()" << endl;
+    int board[MAX_SIZE][MAX_SIZE];
+
+    // test case 1
+
+    string test_board_1[] = {"O-OX",
+                             "OO--",
+                             "X---",
+                             "-O--"};
+    int size_1 = 4;
+    read_board_from_string(board, test_board_1, size_1);
+    cout << board_has_no_threes(board, size_1)  << endl;
+
+    // test case 2
+
+    string test_board_2[] = {"O-OX",
+                             "OO--",
+                             "O---",
+                             "-O--"};
+    int size_2 = 4;
+    read_board_from_string(board, test_board_2, size_2);
+    cout << board_has_no_threes(board, size_2)  << endl;
+
+    // test case 3
+    string test_board_3[] = {"XXXO",
+                             "XO--",
+                             "O---",
+                             "-O--"};
+    int size_3 = 4;
+    read_board_from_string(board, test_board_3, size_3);
+    cout << board_has_no_threes(board, size_3)  << endl;
+}
+
+
 
 // define more test functions here
